73793783dd3926a797d68e389478a994b49a96e0
NathanA100 <168213068+NathanA100@users.noreply.github.com>
2024-08-29 17:20:58 -0400

    Add project files.

---
+++ Lab 03/Lab 03.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+	// The rest of this function is an exercise to the reader
+}
+
+void Addition(float A, float B) {
+	cout << "Answer: " ;
+}
+void Subtraction(float A, float B) {
+	cout << "Answer: ";
+}
+void Multiplication(float A, float B) {
+	cout << "Answer: ";
+}
+void Division(float A, float B) {
+	cout << "Answer: ";
+}
+
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		Addition(A, B);
+		cout<< A<<" + "<<B<<" = "<< A + B << endl;
+	}
+	if (choice == 2) {
+		Subtraction(A, B);
+		cout << A << " - " << B << " = " << A - B << endl;
+	}
+	if (choice == 3) {
+		Multiplication(A, B);
+		cout << A << " * " << B << " = " << A * B << endl;
+	}
+	if (choice == 4) {
+		Division(A, B);
+		cout << A << " / " << B << " = " << A / B << endl;
+	}
+	return 0;
+}
